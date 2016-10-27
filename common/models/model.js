a84846634e481d02177a71cc1da820daaffba505
Nut Kaewnark <nut-boss@hotmail.com>
2016-10-27 06:03:40 -0700

    implemented mysql

---
+++ common/models/model.js
@@ -0,0 +1,9 @@
+// var ds = Model.app.dataSources.mysql_test;
+//  
+// ds.createModel(Book.name, Book.properties);
+//  
+// ds.automigrate(function () {
+//   ds.discoverModelProperties('CUSTOMER_TEST', function (err, props) {
+//     console.log(props);
+//   });
+// });
