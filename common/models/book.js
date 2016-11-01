7f8b1158c1b8e5561fda34f3ebe9491667638d27
nutkaewnak <nut-boss@hotmail.com>
2016-11-01 12:23:51 +0700

    commit from mac osx

--- common/models/book.js
+++ common/models/book.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function(Book) {
-
+  Book.create_naja = function(book, callback){
+    return callback(null, "TEST CREATE");
+  }
+  Book.remoteMethod(
+    "create_naja",
+    {
+      description: "Test create",
+      accepts: [
+        {args: "book", type: "Book", require: true}
+      ],
+      returns: {
+        args: 'status', type:
+      }
+    }
+  );
 };
